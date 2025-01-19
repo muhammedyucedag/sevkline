6bdedd849ef59d2eb4a4df0fb7e16baccccd3699
Muhammed Yücedağ <113437788+muhammedyucedag@users.noreply.github.com>
2025-01-19 17:33:42 +0300

    Migration

---
+++ SevkLine.Infrastructure/Migrations/20250119142017_UPD_Department_Entity_Update.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SevkLine.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UPD_Department_Entity_Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Departments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Departments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Departments",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
